1259b6ff0f1f4239aac3797e693f6be588c319cb
unknown <katykuz@uw.edu>
2019-07-03 12:43:22 -0700

    Added Message Send capability

--- src/App.js
+++ src/App.js
@@ -3,29 +3,63 @@ import React from 'react';
 import './App.css';
 import logo from './logo.png'
 import TextInput from './TextInput'
+import NamePicker from './NamePicker'
+
 
 /* Creating the page and what is in the page */
-function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <img src={logo} className="logo" alt="logo" />
-        Chatbox
+class App extends React.Component {
+  state = {
+    messages: [],
+    name:'',
+    editName:false,
+  }
+
+  gotMessage = (text) => {
+    /* ... is a separator*/
+    var newMessagesArray = [...this.state.messages, text]
+    this.setState({messages: newMessagesArray})
+  }
+
+
+
+  render() {
+    var {messages} = this.state
+    return (
+      <div className="App">
+        <header className="header">
+          <img src={logo} className="logo" alt="logo" />
+          Chatbox
+          <NamePicker />
         </header>
 
-        <div className="container">
-            <p>Hello. How are you today?</p>
-            </div>
-
-        <div class="container darker">
-           <p>Hey! I'm fine. Thanks for asking!</p>
+        {/*<div className="container">
+          <p>Hello. How are you today?</p>
         </div>
-      
-        <TextInput />
 
-    </div>
-  );
+        <div className="container darker">
+          <p>Hey! I'm fine. Thanks for asking!</p>
+    </div>*/}
+
+        <main className="messages">
+          {/* To write javaScript in html, start with curly braces*/}
+          {messages.map((m, i)=>{
+            /*Putting curly braces around m because m is javascript and div is html*/
+            return <div key={i} className="bubble-wrap">
+              <div className="bubble">
+              <span>{m}</span>
+              </div>
+            </div>
+          })}
+
+        </main>
+
+        <TextInput sendMessage={this.gotMessage} />
+
+      </div>
+    );
+  }
 }
+
 
 export default App;
 
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,12 +1,38 @@
 import React from 'react'
+import { FiSend } from "react-icons/fi";
 
 /* Inserting Text Input Box */
 class TextInput extends React.Component{
 
+    state={
+        text:""
+    }
+
+    send = () => {
+        this.props.sendMessage(this.state.text)
+        this.setState({text:""})
+    }
+
+    keyPress = (e) => {
+        if(e.key==='Enter'){
+            this.send()
+        }
+    }
+
     render(){
         return(<div className = "text-input">
-            <input />
-            <button> &#8594; </button>
+            {/* Places instruction text into input, value prop controls the input */}
+            <input value={this.state.text}
+            /*Placeholder is temporary and once you type, it disappears*/
+            placeholder="Write your message here..."
+            /* Gives function to text input, e stands for event (can be a different letter) */
+            onChange={e=> this.setState({text: e.target.value})}
+            /* No fat arrow bellow (onKeyPress) because we made our own KeyPress function that included the fat arrow*/
+            onKeyPress={this.keyPress}
+            />
+            <button disabled={!this.state.text} onClick={this.send} >
+            < FiSend style={{height:12, width:12}}/> 
+            </button>
             </div>)
             }
 
