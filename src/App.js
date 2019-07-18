6f62c7aa17ed3241487f8fe5edb47c178211ade1
unknown <katykuz@uw.edu>
2019-07-17 20:44:05 -0700

    camera and send image functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/storage"
 import Camera from 'react-snap-pic'
+import Div100vh from 'react-div-100vh'
 
 
 /* Creating the page and what is in the page */
@@ -30,7 +31,7 @@ class App extends React.Component {
       authDomain: "summer-project-438.firebaseapp.com",
       databaseURL: "https://summer-project-438.firebaseio.com",
       projectId: "summer-project-438",
-      storageBucket: "",
+      storageBucket: "summer-project-438.appspot.com",
     });
     this.db = firebase.firestore();
 
@@ -65,60 +66,69 @@ class App extends React.Component {
     }
     this.setState({ editName })
   }
-
-  takePicture = (img) => {
-   console.log(img)
-    this.setState({showCamera:false})
-}
-
+  /* Send picture to Firebase storage and send reference to image*/
+  takePicture = async (img) => {
+    this.setState({ showCamera: false })
+    const imgID = Math.random().toString(36).substring(7);
+    var storageRef = firebase.storage().ref();
+    var ref = storageRef.child(imgID + '.jpg');
+    await ref.putString(img, 'data_url') //await states to wait for line to complete before beginning to run the next line//
+    this.send({ img: imgID })
+  }
 
   render() {
     var { editName, messages, name } = this.state
     return (
-      <div className="App">
-        <header className="header">
-          <div className = "iconwrapper"><img src={logo} className="logo" alt="logo" />
-            Chatbox</div>
-          <NamePicker
-            name={name}
-            editName={editName}
-            changeName={name => this.setState({ name })}
-            setEditName={this.setEditName} />
-        </header>
-
-        <main className="messages">
-          {/* To write javaScript in html, start with curly braces*/}
-          {messages.map((m, i) => {
-            /*Putting curly braces around m because m is javascript and div is html*/
-            return <Message key={i} m={m} name={name} />
-          })}
-
-        </main>
-
-        <TextInput 
-        sendMessage={text => this.send({ text })}
-        showCamera={() => this.setState({showCamera:true})}/>
+      <Div100vh className="App">
         {this.state.showCamera && <Camera takePicture={this.takePicture} />}
+          <header className="header">
+            <div className="iconwrapper"><img src={logo} className="logo" alt="logo" />
+              {editName ? '' : 'Chatbox'} </div>
+            <NamePicker
+              name={name}
+              editName={editName}
+              changeName={name => this.setState({ name })}
+              setEditName={this.setEditName}
+            //{editName= ? ''::'logo'} 
+            />
+          </header>
 
-      </div>
 
-    );
+          <main className="messages">
+            {/* To write javaScript in html, start with curly braces*/}
+            {messages.map((m, i) => {
+              /*Putting curly braces around m because m is javascript and div is html*/
+              return <Message key={i} m={m} name={name} />
+            })}
+
+          </main>
+
+          <TextInput
+            sendMessage={text => this.send({ text })}
+            showCamera={() => this.setState({ showCamera: true })}
+          />
+        </Div100vh>
+
+    )
   }
 }
 
 
 export default App;
+const bucket = 'https://firebasestorage.googleapis.com/v0/b/summer-project-438.appspot.com/o/'
+const suffix = '.jpg?alt=media'
 
 function Message(props) {
   var {m, name} = props
   return(<div className="bubble-wrap"
-  from={m.from === name ? "me" : "you"}>
+          from={m.from === name ? "me" : "you"}>
 
-  {m.from !== name && <div className="bubble-name">{m.from}</div>}
-  <div className="bubble">
-    <span>{m.text}</span>
-  </div>
-</div>)
-}
-
-
+          {m.from !== name && <div className="bubble-name">{m.from}</div>}
+          <div className="bubble">
+            <span>{m.text}</span>
+            {m.img && <img alt='pic' src={bucket + m.img + suffix} />}
+          </div>
+        </div>)
+        }
+        
+        
